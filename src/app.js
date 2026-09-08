f2b6f819f56ee9385d5d1c6ac16b1f5e10b0bed6
Nikunj-1812 <nikunjsorathiya712@gmail.com>
2026-09-08 21:16:01 +0530

    Added

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cookieparse = require("cookie-parser")
 const authRouter = require("./routes/auth.routes")
 const accountRouter = require("./routes/account.routes")
+const transactionRouter = require("./routes/transection.routes")
 
 const app = express()
 
@@ -13,5 +14,6 @@ app.use(cookieparse())
 
 app.use("/api/auth",authRouter)
 app.use("/api/accounts",accountRouter)
+app.use("/api/transactions", transactionRouter)
 
 module.exports =app
